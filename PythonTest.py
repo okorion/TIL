303f0eddc14ab9b42f814ae707594c6396e49cff
okorion <ok.or.orion@gmail.com>
2022-01-20 09:55:38 +0900

    temp_save_1.0.34

--- PythonTest.py
+++ PythonTest.py
@@ -11,6 +11,7 @@ number = int(input())
 for i in range(number, -1, -1):
     print(i)
 '''
+'''
 number = int(input())
 x = 1
 sum = 0
@@ -20,3 +21,4 @@ while x < number + 1:
     x = x + 1
 
 print(sum)
+'''
