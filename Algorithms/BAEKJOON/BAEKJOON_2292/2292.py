2f039c00dcd1f49a5b6e812ca39911cf9be54afc
okorion <ok.or.orion@gmail.com>
2022-03-06 04:03:20 +0900

    temp_save_1.2.8

--- Algorithms/BAEKJOON/BAEKJOON_2292/2292.py
+++ Algorithms/BAEKJOON/BAEKJOON_2292/2292.py
@@ -1,1 +1,19 @@
-int(input())
+import sys
+import math
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+tmp = math.ceil((T-1) / 6)
+
+cnt = 0
+idx = 0
+
+for _ in range(0, tmp+1):
+    cnt += _
+    idx += 1
+    if tmp > cnt:
+        pass
+    else:
+        print(idx)
+        break
