dc6040df9ac60ee14a5a9de7a7e4f7fe35637e37
okorion <ok.or.orion@gmail.com>
2022-03-07 02:04:44 +0900

    temp_save_1.2.10

--- Algorithms/BAEKJOON/BAEKJOON_1193/1193.py
+++ Algorithms/BAEKJOON/BAEKJOON_1193/1193.py
@@ -1,7 +1,31 @@
 import sys
 sys.stdin = open('input.txt')
 
-T = int(input())
+X = int(input())
 
-for tc in range(1, T+1):
-    num = int(input())
+t = 0
+K = 0
+M = 0
+
+for _ in range(4773):
+    t += _
+    if X == t:
+        if _ % 2 == 0:
+            print(str(_) + "/1")
+        else:
+            print("1/" + str(_))
+        break
+    else:
+        if X < t:  # X = 8, t = 10
+            if _ % 2 == 0:
+                K = _   # K = 4
+                M = t - X
+                result = str(K - (t - X)) + "/" + str(t - X + 1)
+                print(result)
+                break
+            else:
+                K = _   # K = 4
+                M = t - X
+                result2 = str(t - X + 1) + "/" + str(K - (t - X))
+                print(result2)
+                break
