3b7305331ce4ac674bb3b5b2ba1a07a1056d8313
okorion <ok.or.orion@gmail.com>
2022-03-23 02:34:35 +0900

    temp_save_1.2.22

---
+++ Algorithms/SWEA/1860_진기의 최고급 붕어빵/1860_진기의 최고급 붕어빵.py
@@ -0,0 +1,60 @@
+import sys
+sys.stdin = open("input2.txt")
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M, K = list(map(int, input().split()))
+    customer_time = list(map(int, input().split()))
+    customer_time.sort()
+
+    max0 = K
+    tmp = 0
+    tst = 2  # int(customer_time[0]/M + 1) 로 입력해서 테스트 케이스 997/1000 => 오래 걸림.
+
+    error = 0
+
+    for _ in customer_time:
+        _ = int(_/M)
+        tmp += 1
+        if _ >= tst:
+            tst += 1
+            max0 += K
+
+        elif max0 - tmp < 0:
+            error += 1
+
+        elif int(customer_time[0]/M) == 0:
+            error += 1
+
+        elif max(customer_time) > 11111:
+            error += 1
+
+    if error == 0:
+        print(f'#{tc} Possible')
+
+    else:
+        print(f'#{tc} Impossible')
+
+
+
+    # if star == N/K:
+    #     print
+
+    # for _ in customer_time:
+    #     _ = int(_/M)
+    #
+    #     if _ == star:
+    #         tmp += 1
+    #         if tmp > max0:
+    #             print("Impossible")
+    #             break
+    #     elif _ == 0:
+    #         print("Impossible")
+    #         break
+    #     else:
+    #         max0 += K
+    #         tmp += 1
+    #
+    # if tmp == len(customer_time):
+    #     print("Possible")
